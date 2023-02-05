19808409e6586bbcf95baad4741df894b6b55e0c
Rafael Moraes <rafaelsm003@gmail.com>
2023-02-05 18:31:17 -0300

    refactor: cria função auxiliar get_line_info para reduzir a complexidade de exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,35 +1,41 @@
 from ting_file_management.queue import Queue
 
 
+def get_line_info(word, file_lines, occurrences, show_content):
+    add_file = False
+    for j in range(len(file_lines)):
+        if word.lower() in file_lines[j].lower():
+            line_info = {"linha": j + 1}
+
+            if show_content:
+                line_info["conteudo"] = file_lines[j]
+
+            occurrences.append(line_info)
+            add_file = True
+    return add_file
+
+
 def exists_word(word, instance: Queue, show_content=False):
-    word_occurrencies = []
+    word_occurrences = []
 
     for i in range(len(instance)):
         file = instance.search(i)
         file_lines = file["linhas_do_arquivo"]
-        occurencies = []
+        occurrences = []
         add_file = False
 
-        for j in range(len(file_lines)):
-            if word.lower() in file_lines[j].lower():
-                line_info = {"linha": j + 1}
-
-                if show_content:
-                    line_info["conteudo"] = file_lines[j]
-
-                occurencies.append(line_info)
-                add_file = True
+        add_file = get_line_info(word, file_lines, occurrences, show_content)
 
         if add_file:
-            word_occurrencies.append({
+            word_occurrences.append({
                 "palavra": word,
                 "arquivo": file["nome_do_arquivo"],
-                "ocorrencias": occurencies
+                "ocorrencias": occurrences
             })
 
-    return word_occurrencies
+    return word_occurrences
 
 
 def search_by_word(word, instance):
-    word_occurrencies = exists_word(word, instance, show_content=True)
-    return word_occurrencies
+    word_occurrences = exists_word(word, instance, show_content=True)
+    return word_occurrences
