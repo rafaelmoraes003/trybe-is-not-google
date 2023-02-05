b3b0945920798caef2913fdbb3188d3f0d15c980
Rafael Moraes <rafaelsm003@gmail.com>
2023-02-05 11:40:40 -0300

    feat: implementa a função process, capaz de transformar o conteúdo da lista gerada pela função txt_importer em um dicionário que será armazenado dentro da Queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if len(instance) > 0:
+        for i in range(len(instance)):
+            if instance.search(i)["nome_do_arquivo"] == path_file:
+                return
+
+    file_content = txt_importer(path_file)
+
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content,
+    }
+
+    print(file_info)
+    instance.enqueue(file_info)
 
 
 def remove(instance):
